7bd5d516d5eeb6c228cc4334298aa66c3889392e
thesamtang <linglinghost@gmail.com>
2014-09-20 11:11:51 -0400

    added dummy health indicators

--- src/app.js
+++ src/app.js
@@ -6,56 +6,49 @@
  */
 
 var UI = require('ui');
-//var Vector2 = require('vector2');
+var Vector2 = require('vector2');
 
-var main = new UI.Card({
+/*
+var healthWindow = new UI.Card({
   title: 'Pebble.js',
   icon: 'images/menu_icon.png',
   subtitle: 'Hello World!',
   body: 'Press any button.'
 });
 
-main.show();
-
-/*
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+healthWindow.show();
 */
+var mainWindow = new UI.Window({
+  fullscreen: true
+});
+
+var playerBkgd = new UI.Rect({
+  backgroundColor: "white",
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 84)
+});
+
+//var playerHealth; //get from iOS
+var playerHealthBar = new UI.Text({
+  position: new Vector2(0, 21),
+  size: new Vector2(144, 84),
+  font: 'bitham-42-medium-numbers',
+  color: 'black',
+  text: '100', //playerHealth,
+  textAlign: 'center'
+});
+
+//var opponentHealth; //get from iOS
+var opponentHealthBar = new UI.Text({
+  position: new Vector2(0, 105),
+  size: new Vector2(144, 84),
+  font: 'bitham-42-medium-numbers',
+  color: 'white',
+  text: '100', //opponentHealth,
+  textAlign: 'center'
+});
+
+mainWindow.add(playerBkgd);
+mainWindow.add(playerHealthBar);
+mainWindow.add(opponentHealthBar);
+mainWindow.show();
